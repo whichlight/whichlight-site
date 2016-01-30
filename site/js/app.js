0280713dd217791fa5825783957ec306c7d28004
Kawandeep Virdee <whichlight@gmail.com>
2016-01-30 09:19:16 -0500

    updates

---
+++ site/js/app.js
@@ -0,0 +1,39 @@
+
+$(document).ready(function(){
+  setInterval(function(){
+  logoAnimate()},500);
+});
+
+
+var logoAnimate = function(){
+  c+=0.01;
+  c%=1;
+  var col = HSVtoRGB(1,c,1);
+  $('.circle').css('background','rgb('+[col.r,col.g,col.b].join(',')+')');
+}
+
+
+function HSVtoRGB(h, s, v) {
+    var r, g, b, i, f, p, q, t;
+    if (arguments.length === 1) {
+        s = h.s, v = h.v, h = h.h;
+    }
+    i = Math.floor(h * 6);
+    f = h * 6 - i;
+    p = v * (1 - s);
+    q = v * (1 - f * s);
+    t = v * (1 - (1 - f) * s);
+    switch (i % 6) {
+        case 0: r = v, g = t, b = p; break;
+        case 1: r = q, g = v, b = p; break;
+        case 2: r = p, g = v, b = t; break;
+        case 3: r = p, g = q, b = v; break;
+        case 4: r = t, g = p, b = v; break;
+        case 5: r = v, g = p, b = q; break;
+    }
+    return {
+        r: Math.round(r * 255),
+        g: Math.round(g * 255),
+        b: Math.round(b * 255)
+    };
+}
